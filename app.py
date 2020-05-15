14f59b7eb26425ecfdf6659d6a06db6b15011a89
SanteauX <hugo.joncour.k2o@gmail.com>
2020-05-15 01:44:52 -0400

    added everything before trying to use FlaskForm again

--- app.py
+++ app.py
@@ -33,12 +33,23 @@ class User(db.Model):
     answer = db.Column(db.String(1000))
     date_created = db.Column(db.DateTime, default=datetime.now)
 
+class Upload(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    picture = db.Column(db.String(50))
+    description = db.Column(db.String(1000))
+    date_created = db.Column(db.DateTime, default=datetime.now)
+
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    password = PasswordField("password", validators=[InputRequired(), Length(max=80)])
+
 ########################### FUNCTIONS
 def listOfUsers():
     user_lines = User.query.all()
     lines = []
     for i in range(0, len(user_lines)):
-        line = [user_lines[i].id + 1000000000, user_lines[i].name,user_lines[i].email, user_lines[i].password, user_lines[i].question,user_lines[i].answer,user_lines[i].date_created]
+        line = [user_lines[i].id, user_lines[i].name,user_lines[i].email, user_lines[i].password, user_lines[i].question,user_lines[i].answer,user_lines[i].date_created]
         lines.append(line)
     return lines
 
@@ -46,40 +57,47 @@ def listOfUsers():
 @login_manager.user_loader
 def load_user(email):
     accounts = listOfUsers()
-    for i in range(0, len(listOfUsers)):
-        if email == listOfUsers[i][2]:
-            return User(listOfUsers[i][0], listOfUsers[i][1], listOfUsers[i][2], listOfUsers[i][3], listOfUsers[i][4], listOfUsers[i][5], listOfUsers[i][6])
+    for i in range(0, len(accounts)):
+        if email == accounts[i][2]:
+            user = User(accounts[i][1], accounts[i][2], accounts[i][3], accounts[i][4], accounts[i][5])
+            return user
 
 def email_exists(email):
     accounts = listOfUsers()
     for i in range(0, len(accounts)):
-        print(accounts[i])
         if email == accounts[i][2]:
             return True
     return False
 
 def check_password(email, password):
     accounts = listOfUsers()
-    for i in range(0, len(listOfUsers)):
-        if email == listOfUsers[i][2]:
-            if password == listOfUsers[i][3]:
+    for i in range(0, len(accounts)):
+        if email == accounts[i][2]:
+            if password == accounts[i][3]:
+                print("password true")
                 return True
+            print("password false")
             return False
+    print("account not found with email")
     return False
+
+########################### CREATE USER FROM FORM
+def create_user(req):
+    name = req["name"]
+    email = req.get("email")
+    password = request.form["password"]
+    question = request.form["question"]
+    answer = request.form["answer"]
+    salt = bcrypt.gensalt()
+    password = bcrypt.hashpw(password.encode(), salt)
+    user = User(name=name, email=email, password=password, question=question, answer=answer)
+    return user
 
 ########################### ACCOUNT
 @app.route('/signup', methods=["POST", "GET"])
 def signup():
     if request.method == "POST":
-        req = request.form
-        name = req["name"]
-        email = req.get("email")
-        password = request.form["password"]
-        question = request.form["question"]
-        answer = request.form["answer"]
-        salt = bcrypt.gensalt()
-        password = bcrypt.hashpw(password.encode(), salt)
-        user = User(name=name, email=email, password=password, question=question, answer=answer)
+        user = create_user(request.form)
         db.session.add(user)
         db.session.commit()
         return redirect(request.url)
@@ -87,16 +105,13 @@ def signup():
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
-    if request.method == "POST":
-        req = request.form
-        email = req.get("email")
-        password = req["password"]
-        salt = bcrypt.gensalt()
-        password = bcrypt.hashpw(password.encode(), salt)
-        accounts = listOfUsers()
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
         if email_exists(email):
-            if check_password(email, password):
-                user = load_user(email)
+            user = load_user(email)
+            session['name'] = user.name
+            if bcrypt.checkpw(form.password.data.encode(), user.password.encode()):
                 login_user(user)
                 return redirect('account', name=user.name)
     return render_template('account/login.html')
